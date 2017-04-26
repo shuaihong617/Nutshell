e88d601b5b1d8094692a079b8c9bd2864173446e
shuaihong617 <shuaihong617@qq.com>
2017-04-26 16:59:33 +0800

    更正接收器参数约束

--- Nutshell.RabbitMQ/RabbitMQReceiver.cs
+++ Nutshell.RabbitMQ/RabbitMQReceiver.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Text;
 using Nutshell.Aspects.Locations.Contracts;
 using Nutshell.Communication;
-using Nutshell.Data;
 using Nutshell.Extensions;
+using Nutshell.IO.Aspects.Locations.Contracts;
 using Nutshell.Messaging.Models;
 using Nutshell.RabbitMQ.Models;
 using Nutshell.Serializing.Xml;
@@ -15,20 +14,20 @@ using RabbitMQ.Client.Events;
 
 namespace Nutshell.RabbitMQ
 {
-        public class RabbitMQReceiver<T>:RabbitMQActor<T>,IReceiver<T>,IStorable<RabbitMQReceiverModel> where T : MessageModel
-	{
-
-                public RabbitMQReceiver(string id = "") 
-			: base( id)
+        public class RabbitMQReceiver<T> : RabbitMQActor<T>, IReceiver<T>, IStorable<RabbitMQReceiverModel>
+                where T : MessageModel
+        {
+                public RabbitMQReceiver(string id = "")
+                        : base(id)
                 {
                 }
 
 
-	        private EventingBasicConsumer _consumer;
+                private EventingBasicConsumer _consumer;
 
-		private readonly RabbitMQQueue _queue = new RabbitMQQueue();
+                private readonly RabbitMQQueue _queue = new RabbitMQQueue();
 
-                public static RabbitMQReceiver<T> Load([MustNotEqualNullOrEmpty]string fileName)
+                public static RabbitMQReceiver<T> Load([MustFileExist] string fileName)
                 {
                         var bytes = XmlStorager.Instance.Load(fileName);
                         var model = XmlSerializer<RabbitMQReceiverModel>.Instance.Deserialize(bytes);
@@ -37,68 +36,66 @@ namespace Nutshell.RabbitMQ
                         receiver.Load(model);
                         return receiver;
                 }
+
                 public void Load(RabbitMQReceiverModel model)
-		{
-			base.Load(model);
+                {
+                        base.Load(model);
                         _queue.Load(model.RabbitMQQueueModel);
                 }
 
-		public void Save(RabbitMQReceiverModel model)
-		{
-			_queue.Save(model.RabbitMQQueueModel);
-			base.Save(model);
-		}
+                public void Save(RabbitMQReceiverModel model)
+                {
+                        _queue.Save(model.RabbitMQQueueModel);
+                        base.Save(model);
+                }
 
-	        /// <summary>
-	        /// 执行启动过程的具体步骤.
-	        /// </summary>
-	        /// <returns>
-	        /// 成功返回True, 否则返回False.
-	        /// </returns>
-	        /// <remarks>
-	        /// 若启动过程有多个步骤, 遇到返回错误的步骤立即停止向下执行.
-	        /// </remarks>
-	        protected override Result StartCore()
-	        {
-		        var baseResult = base.StartCore();
-		        if (!baseResult.IsSuccessed)
-		        {
-			        return baseResult;
-		        }
+                /// <summary>
+                ///         执行启动过程的具体步骤.
+                /// </summary>
+                /// <returns>
+                ///         成功返回True, 否则返回False.
+                /// </returns>
+                /// <remarks>
+                ///         若启动过程有多个步骤, 遇到返回错误的步骤立即停止向下执行.
+                /// </remarks>
+                protected override Result StartCore()
+                {
+                        var baseResult = base.StartCore();
+                        if (!baseResult.IsSuccessed)
+                        {
+                                return baseResult;
+                        }
 
-		        Channel.QueueDeclare(_queue.Name, _queue.IsDurable, _queue.IsExclusive, _queue.IsAutoDelete, null);
-			
-			_consumer = new EventingBasicConsumer(Channel);
-			_consumer.Received += (model, ea) =>
-			{
-				var body = ea.Body;
-				var messageModel = Serializer.Deserialize(body);
+                        Channel.QueueDeclare(_queue.Name, _queue.IsDurable, _queue.IsExclusive, _queue.IsAutoDelete,
+                                null);
 
-				Trace.WriteLine(DateTime.Now.ToChineseLongMillisecondString() + messageModel.Id);
-				
-				OnReceiveSuccessed(new ValueEventArgs<T>(messageModel));
-			};
-			Channel.BasicConsume(_queue.Name,
-					     noAck: true,
-					     consumer: _consumer);
+                        _consumer = new EventingBasicConsumer(Channel);
+                        _consumer.Received += (model, ea) =>
+                        {
+                                var body = ea.Body;
+                                var messageModel = Serializer.Deserialize(body);
 
-			return Result.Successed;
-	        }
+                                Trace.WriteLine(DateTime.Now.ToChineseLongMillisecondString() + messageModel.Id);
+
+                                OnReceiveSuccessed(new ValueEventArgs<T>(messageModel));
+                        };
+                        Channel.BasicConsume(_queue.Name, true, _consumer);
+
+                        return Result.Successed;
+                }
 
 
-		
+                public event EventHandler<ValueEventArgs<T>> ReceiveSuccessed;
 
-		public event EventHandler<ValueEventArgs<T>> ReceiveSuccessed;
+                /// <summary>
+                ///         引发启动事件。
+                /// </summary>
+                /// <param name="e">The <see cref="EventArgs" /> Itance containing the event data.</param>
+                protected virtual void OnReceiveSuccessed(ValueEventArgs<T> e)
+                {
+                        e.Raise(this, ref ReceiveSuccessed);
+                }
 
-		/// <summary>
-		///         引发启动事件。
-		/// </summary>
-		/// <param name="e">The <see cref="EventArgs" /> Itance containing the event data.</param>
-		protected virtual void OnReceiveSuccessed(ValueEventArgs<T> e)
-		{
-			e.Raise(this, ref ReceiveSuccessed);
-		}
-
-		public event EventHandler<ValueEventArgs<Exception>> ReceiveFailed;
+                public event EventHandler<ValueEventArgs<Exception>> ReceiveFailed;
         }
 }
